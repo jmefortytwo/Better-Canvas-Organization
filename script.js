d311814c6fdc35e0b80964a5d94a3e559a599554
jmefortytwo <jackelliottclimbs@gmail.com>
2025-10-06 19:45:06 -0400

    Fetches assignment data for a specific course (can fetch course IDs later), displays them as dynamically updated HTML.

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-const maindiv = document.getElementById("main");
+function removeOriginals() {
+	const maindiv = document.getElementById("main");
+	const divs = maindiv.querySelectorAll('div');
 
-const divs = maindiv.querySelectorAll('div');
+	divs.forEach(div  => {
+		div.remove();
+	});
+}
 
-divs.forEach(div  => {
-	div.remove();
-});
+async function fetchAssignments() {
+	const result = await fetch("https://oglethorpe.instructure.com/api/v1/courses/6338/assignments");
+	const data = await result.json();
+	
+	console.log("fetch successful");
+	return data;
+}
 
+async function displayAssignments() {
+	const assignments = await fetchAssignments();
+	const maindiv = document.getElementById("main");
 
+	assignments.forEach(assignment => {
+		maindiv.innerHTML += '\n<div>'+assignment.name+'</div>';
+	});
+
+}
+
+removeOriginals();
+displayAssignments();
