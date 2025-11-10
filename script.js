6a1b3a9aa7d79ceece6ba61f3a8524a34a30079b
jmefortytwo <jackelliottclimbs@gmail.com>
2025-11-10 12:06:57 -0500

    Cleaned up some stuff and added a very ugly and basic calendar which has no features. Also, removed jsCalendar as it isn't used.

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 function removeOriginals() {
 	const maindiv = document.getElementById("main");
-	const divs = maindiv.querySelectorAll('div');
-
-	for(div of divs) {
-		div.remove();
-	}
+	maindiv.innerHTML = "";
+	maindiv.style = "display:flex";
 }
 
 async function fetchCourses() {
@@ -21,32 +18,74 @@ async function fetchAssignments(id) {
 
 async function processCourses(courses) {
 	let courseData = [];
+	const maindiv = document.getElementById("main");
+	maindiv.innerHTML += "\n<div id = 'biglist' style = 'display:inline-block'></div>";
 
 	for(course of courses) {
 		if(Date.now() < new Date(course.end_at).getTime()) {
 			assignments = await fetchAssignments(course.id);
-			courseData.push({id : course.id, assignments : assignments});
+			courseData.push({id : course.id, name : course.name, assignments : assignments});
 		}
 	}
 
 	return courseData;
 }
 
-async function displayAssignments(assignments) {
-	const maindiv = document.getElementById("main");
+async function displayAssignments(name,assignments) {
+	const biglist = document.getElementById("biglist");
+	biglist.innerHTML += "\n<div id = '"+name+"'></div>";
+	const list = document.getElementById(name);
+
+	const color = Math.floor(Math.random() * 16777215).toString(16);
 
 	for(assignment of assignments) {
-		maindiv.innerHTML += '\n<div>'+assignment.name+'</div>';
+		list.innerHTML += "\n<div style = 'cursor:grab;background-color:#"+color+";margin:2px'>"+assignment.name+"</div>";
 	}
 }
 
+function createCalendar() {
+	let today = new Date();
+	const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+	const maindiv = document.getElementById("main");
+	maindiv.innerHTML += "\n<div id = 'calendar' style = 'display:inline-block'></div>";
+	const calendar = document.getElementById("calendar");
+	calendar.innerHTML += "\n<div>"+monthNames[today.getMonth()]+"</div>\n<div id = 'days'></div>";
+	const days = document.getElementById("days");
+	let length;
+
+	if(today.getMonth() == 1) {
+		if(today.getFullYear() % 4 == 0) {
+			length = 29;
+		} else{
+			length = 28;
+		}
+	} else if(today.getMonth() % 2 == 0) {
+		length = 31;
+	} else {
+		length = 30;
+	}
+	console.log(length);
+
+	for(i = 0; i < length; i++) {
+		console.log(i);
+		if(i == today.getDate()) {
+			days.innerHTML += "<p style = 'display:inline-block;color:red;width:100px;height:100px;outline:2px solid black'>   "+i+"</p>";
+		} else {
+			days.innerHTML += "<p style = 'display:inline-block;width:100px;height:100px;outline:2px solid black'>   "+i+"</p>";
+		}
+	}
+
+}
 async function main() {
 	removeOriginals();
 
 	const courses = await fetchCourses();
 	const courseData = await processCourses(courses);
 	for(course of courseData) {
-		displayAssignments(course.assignments);
+		displayAssignments(course.name, course.assignments);
 	}
+
+	createCalendar();
 }
 main();
